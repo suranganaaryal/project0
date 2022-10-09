59923efe06ddf47ec5be5f84939b26b40f8a6ebc
suranganaaryal <surangana.aryal@bison.howard.edu>
2022-10-09 08:30:43 -0400

    changing positions 1

--- project0.s
+++ project0.s
@@ -4,3 +4,15 @@ sid: .asciiz "@03069961"
 
 main:
 la $t0, sid
+
+li $v0,4
+la $a0,sid
+syscall
+
+lb $t1, 6($t0)
+lb $t2, 0($t0)
+sb $t1, 0($t0)
+sb $t2, 6($t0)
+lb $t1, 7($t0)
+lb $t2, 1($t0)
+sb $t1, 1($t0)
